ae329f85564e39385f03ee219d67bc1e9c3e0472
Prabhu Murthy <prabhu.m.murthy@gmail.com>
2023-04-10 14:44:24 +0530

    updating rollup config

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,34 +2,54 @@ import autoprefixer from 'autoprefixer';
 import cssNano from 'cssnano';
 import presetENV from 'postcss-preset-env';
 import commonjs from 'rollup-plugin-commonjs';
-import resolve from 'rollup-plugin-node-resolve';
+import del from 'rollup-plugin-delete';
 import postcss from 'rollup-plugin-postcss';
 import { terser } from 'rollup-plugin-terser';
 import typescript from 'rollup-plugin-typescript2';
 import pkg from './package.json' assert { type: 'json' };
 
 const config = {
-  input: 'src/index.tsx',
+  input: 'src/react-turbo-image.tsx',
   output: {
-    file: 'dist/bundle.js',
+    file: 'dist/react-turbo-image.js',
     format: 'esm',
     sourcemap: true,
     name: pkg.name,
+    banner: () => `/*!
+    * ${pkg.name} v${pkg.version}
+    * (c) ${new Date().getFullYear()} Prabhu Murthy
+    * License: MIT
+    */`,
   },
   plugins: [
+    del({ targets: 'dist/*' }),
+    commonjs({
+      include: 'node_modules/**',
+    }),
     typescript({
       target: 'es2021',
       declaration: true,
       declarationDir: 'dist',
+      exclude: ['node_modules/**', 'src/**/*.test.tsx', 'src/App.tsx', 'src/main.tsx'],
     }),
-    commonjs({
-      include: 'node_modules/**',
-    }),
-    // resolve({
-    //   extensions: ['.jsx', '.tsx', '.ts'],
-    // }),
     postcss({
-      plugins: [autoprefixer, cssNano, presetENV],
+      extract: true,
+      minimize: true,
+      name: 'react-turbo-image.css',
+      plugins: [
+        autoprefixer,
+        cssNano({
+          preset: [
+            'default',
+            {
+              discardComments: {
+                removeAll: true,
+              },
+            },
+          ],
+        }),
+        presetENV,
+      ],
     }),
     terser({
       compress: {
